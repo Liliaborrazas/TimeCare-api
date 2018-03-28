619d7d63eff7545300da7cd0aff219058df6a9d9
Lilia Borrazas <liliaborrazas@MacBook-Pro-de-Lilia.local>
2018-03-28 09:55:14 +0200

    feat:create event.model,event.route and event.controller

---
+++ models/event.model.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+
+const eventSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: [true, 'The title is required']
+    },
+    description: {
+      type: String
+    },
+
+    price: {
+        type: Number,
+        min: 0,
+        default: 0
+      },
+      days: [{
+        type: String,
+        enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+      }],
+
+      startTime: {
+        type: Number,
+        required: [true, 'Start time is required']
+      },
+      endTime: {
+        type: Number,
+        required: [true, 'End time is required']
+      },
+    }, { 
+        timestamps: true,
+        toJSON: {
+          transform: (doc, ret) => {
+            ret.id = doc._id;
+            delete ret._id;
+            delete ret.__v;
+            return ret;
+          }
+        }
+      });
+
+const Event = mongoose.model('Event', EventSchema);
+module.exports = Event;
